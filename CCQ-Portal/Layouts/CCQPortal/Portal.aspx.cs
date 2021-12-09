4f66833b74c47f8aefa1639b3c1a59f4c0a1dcde
Husein Shabbir Rajkotwala <Husein.Rajkotwala@ccq.edu.qa>
2021-12-09 09:07:00 +0300

    Commiting FeetBack and API changes

---
+++ CCQ-Portal/Layouts/CCQPortal/Portal.aspx.cs
@@ -0,0 +1,59 @@
+﻿
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace CCQ_Portal.Layouts.CCQPortal
+{
+    public partial class Portal : LayoutsPageBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+
+
+        //public static string AddFeedBack(string param)
+        //{
+        //    string strResponse = "";
+        //    try
+        //    {
+        //        string strUrl = SPContext.Current.Site.Url;
+
+        //        SPSecurity.RunWithElevatedPrivileges(delegate ()
+        //        {
+        //            using (SPSite objSite = new SPSite(strUrl))
+        //            {
+        //                using (SPWeb objWeb = objSite.OpenWeb())
+        //                {
+        //                    objWeb.AllowUnsafeUpdates = true;
+        //                    SPList objList = objWeb.Lists["CCQFeedback"];
+        //                    SPListItem item = objList.Items.Add();
+        //                    item["Title"] = "using C# :Add new list item programmatically";
+        //                    item.Update();
+
+        //                    strResponse = "Success";
+        //                    objWeb.AllowUnsafeUpdates = false;
+
+        //                }
+        //            }
+        //        });
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        strResponse = "Failed";
+
+
+        //    }
+        //    return strResponse;
+        //}
+        [WebMethod]
+        public static string CurrentlogInUserName()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ CCQ-Portal/Layouts/CCQPortal/PortalMethods.aspx.cs
@@ -0,0 +1,81 @@
+﻿
+
+
+using CCQDTO;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace CCQ_Portal.Layouts.CCQPortal
+{
+
+    public partial class PortalMethods : LayoutsPageBase
+    {
+    
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        [WebMethod]
+       
+        public static string AddFeedBackInfo(List<FeedBack> FeedBack)
+
+        {
+            string strResponse = "";
+            if (FeedBack != null)
+
+            {
+
+
+                try
+                {
+                    string strUrl = SPContext.Current.Site.Url;
+
+                    SPSecurity.RunWithElevatedPrivileges(delegate ()
+                    {
+                        using (SPSite objSite = new SPSite(strUrl))
+                        {
+                            using (SPWeb objWeb = objSite.OpenWeb())
+                            {
+                                objWeb.AllowUnsafeUpdates = true;
+                                SPList objList = objWeb.Lists["CCQFeedback"];
+                                SPListItem item = objList.Items.Add();
+                                item["Title"] =FeedBack[0].Title;
+                                item["Name"] = FeedBack[0].Name;
+
+                                item["Email"] = FeedBack[0].Email;
+
+                                item["Phone"] = FeedBack[0].Phone;
+                                item["Comments"] = FeedBack[0].Comments;
+                                item["Subject"] = FeedBack[0].Subject;
+
+                                item["Category"] = new SPFieldLookupValue(FeedBack[0].CategoryId);
+
+                                item.Update();
+
+                                strResponse = "Success";
+                                objWeb.AllowUnsafeUpdates = false;
+
+                            }
+                        }
+                    });
+                }
+                catch (Exception ex)
+                {
+                    strResponse = "Failed";
+
+
+                }
+
+            }
+
+            return strResponse;
+
+        }
+
+    }
+}
